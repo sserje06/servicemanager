26221d7fe078c380f51853c5c5b762cfed3181e2
sserje06 <sserje06@gmail.com>
2017-09-26 22:24:17 -0500

    Menu Nuevo

--- app/src/main/java/apps4life/servicemanager/PostActivity.java
+++ app/src/main/java/apps4life/servicemanager/PostActivity.java
@@ -2,8 +2,13 @@ package apps4life.servicemanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PostActivity extends AppCompatActivity {
+
+
+    private TextView tv1;
+    private TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
