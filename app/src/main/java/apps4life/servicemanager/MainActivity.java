0a6d620d66831b3446fba7102010016c6a591586
sserje06 <sserje06@gmail.com>
2017-10-10 00:31:41 -0500

    Se agrega Clase para cargar imagenes - Pendiente por arreglarla

--- app/src/main/java/apps4life/servicemanager/MainActivity.java
+++ app/src/main/java/apps4life/servicemanager/MainActivity.java
@@ -1,11 +1,14 @@
 package apps4life.servicemanager;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -17,10 +20,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 import static apps4life.servicemanager.R.layout.content_main;
 
@@ -28,6 +36,9 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     FirebaseRecyclerAdapter mAdapter;
+    private RecyclerView lettersView;
+    FirebaseDatabase database;
+    DatabaseReference imagesLettersdb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,7 +76,61 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Carga las imagenes de Cartas de la organización
+        lettersView = (RecyclerView)findViewById(R.id.rvCartas);
+        lettersView.setHasFixedSize(true);
+        lettersView.setLayoutManager(new LinearLayoutManager(this));
+
+        database = FirebaseDatabase.getInstance();
+        imagesLettersdb = database.getReference("tblPathUploads");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Letters_Model, Letter_Holder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Letters_Model, Letter_Holder>(Letters_Model.class,
+                        R.layout.design_rowl_letters,
+                        Letter_Holder.class,
+                        imagesLettersdb) {
+
+                    @Override
+                    protected void populateViewHolder(Letter_Holder viewHolder, Letters_Model model, int position) {
+                    viewHolder.setPathName(model.getPathname());
+                    viewHolder.setPathRoute(getApplicationContext(), model.getPathroute());
+                    }
+                };
+                lettersView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    //ViewHolder para las imagenes de Cartas de la Organización
+    public static class Letter_Holder extends RecyclerView.ViewHolder{
+        View mView;
+
+        public Letter_Holder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent browserIntent = new Intent(Intent.ACTION_VIEW,
+                            Uri.parse("www.google.com"));
+                    Intent browserChooserIntent = Intent.createChooser(browserIntent,"Escoge el navegador");
+                    v.getContext().startActivity(browserChooserIntent);
+                }
+            });
+        }
+            public void setPathName(String title){
+                TextView post_title = (TextView)mView.findViewById(R.id.text_view_rowl_letters);
+                post_title.setText(title);
+            }
+            public void setPathRoute(Context ctx, String image){
+                ImageView post_image = (ImageView)mView.findViewById(R.id.image_view_rowl_letters);
+                Picasso.with(ctx).load(image).into(post_image);
+            }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -109,7 +174,6 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-        Fragment fragment;
 
         if (id == R.id.nav_home) {
             // Handle the camera action
