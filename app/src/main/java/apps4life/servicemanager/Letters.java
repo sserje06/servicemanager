aaf55f1055ad34eb9d0b15fc5c300fac1ce2f26c
sserje06 <sserje06@gmail.com>
2017-10-13 19:45:51 -0500

    Piccasso add

--- app/src/main/java/apps4life/servicemanager/Letters.java
+++ app/src/main/java/apps4life/servicemanager/Letters.java
@@ -1,40 +1,45 @@
 package apps4life.servicemanager;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Letters extends AppCompatActivity {
 
-    FirebaseRecyclerAdapter mAdapter;
+    private Context context;
+    private DatabaseReference dbImages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_letters);
+        initActivity();
+    }
 
-        DatabaseReference dbImages =
-                FirebaseDatabase.getInstance().getReference().child("tblPathUploads");
+    private void initActivity() {
+        context = this;
+        dbImages = FirebaseDatabase.getInstance().getReference().child("tblPathUploads");
+        setupRecycler();
+    }
 
+    private void setupRecycler() {
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rvCartas);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        mAdapter =
-                new FirebaseRecyclerAdapter<Letters_Model, LettersHolder>(
-                        Letters_Model.class,R.layout.design_rowl_letters,LettersHolder.class, dbImages)
-                {
-
-                    @Override
-                    protected void populateViewHolder (LettersHolder letterholder, Letters_Model lettermodels, int position){
-                        letterholder.setPathName(lettermodels.getPathname());
-                        letterholder.setPathRoute(getApplicationContext(), lettermodels.getPathroute());
-                    }
-                };
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        FirebaseRecyclerAdapter mAdapter = new FirebaseRecyclerAdapter<Letters_Model, LettersHolder>(
+                Letters_Model.class,R.layout.design_rowl_letters,LettersHolder.class, dbImages
+        ){
+            @Override
+            protected void populateViewHolder (LettersHolder letterholder, Letters_Model lettermodels, int position){
+                letterholder.setPathName(lettermodels.getPathname());
+                letterholder.setPathRoute(context, lettermodels.getPathroute());
+            }
+        };
         recyclerView.setAdapter(mAdapter);
     }
+
 }
--- app/src/main/java/apps4life/servicemanager/LettersHolder.java
+++ app/src/main/java/apps4life/servicemanager/LettersHolder.java
@@ -3,7 +3,9 @@ package apps4life.servicemanager;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,9 +31,35 @@ public class LettersHolder extends RecyclerView.ViewHolder {
         post_title.setText(pathname);
     }
 
-    public void setPathRoute(Context ctx, String pathrouth) {
+    public void setPathRoute(final Context ctx, final String pathrouth) {
         ImageView post_image = (ImageView) mView.findViewById(R.id.image_view_rowl_letters);
         Glide.with(ctx).load(pathrouth).into(post_image);
+        post_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setupDialog(ctx, pathrouth);
+            }
+        });
+    }
+
+    private void setupDialog(Context context, String path) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.dialog_image_full, null);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.setCancelable(true);
+        ImageView img_photo = (ImageView)view.findViewById(R.id.img_photo);
+        TextView lbl_close = (TextView)view.findViewById(R.id.lbl_close);
+        //Glide.with(context).load(R.drawable.ic_alert).into(img_photo);
+        Picasso.with(context).load(path).placeholder(R.drawable.ic_alert).into(img_photo);
+        lbl_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.show();
     }
 
 }
